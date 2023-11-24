aa6dc0939f1108f5b9ae62a40c435888a567ec10
Tatiana <tatiana@Tatianas-MacBook-Pro.local>
2023-11-23 21:31:07 -0600

    feat: setting appollo

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
