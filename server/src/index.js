8458f171a8d55259dd57ca0534494d29f42d016d
Tatiana <tatiana@Tatianas-MacBook-Pro.local>
2024-01-14 21:52:40 -0600

    fix: fix apollo instalation error

--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,48 @@
 const { ApolloServer } = require('apollo-server');
 
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`
+const fs = require('fs');
+const path = require('path');
+
+
+let tasks = [{
+    id: 'task-0',
+    status: 'complete',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
 
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    tasks: () => tasks,
+  },
+  Mutation:{
+    task: (parent, args) => {
+  
+      let idCount = tasks.length
+  
+         const task = {
+          id: `task-${idCount++}`,
+          description: args.description,
+          status: args.status,
+        }
+        tasks.push(task)
+        return task
+      }
   }
 }
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
+  const server = new ApolloServer({
+    typeDefs: fs.readFileSync(
+      path.join(__dirname, 'schema.graphql'),
+      'utf8'
+    ),
+    resolvers,
+  })
 
-server
-  .listen()
-  .then(({ url }) =>
-    console.log(`Server is running on ${url}`)
-  );
+
+  
+  server
+    .listen()
+    .then(({ url }) =>
+      console.log(`Server is running on ${url}`)
+    );
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+
+
+// 1
 import {
   ApolloProvider,
   ApolloClient,
@@ -10,28 +12,22 @@ import {
   InMemoryCache
 } from '@apollo/client';
 
+// 2
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
 
-
+// 3
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
 });
 
-
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
+// 4
+ReactDOM.render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ 
